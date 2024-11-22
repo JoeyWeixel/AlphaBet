373fa310a2afb9033f72a4252b9d37fcdef8890f
JoeyWeixel <joeyweixel@gmail.com>
2024-11-22 12:52:08 -0500

    refactor sln file to remove bugs

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ac4f373768d7bde4602bbc2f8af49fd23e94303")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42721ac24a175ebab3a473d0a0e2b21145426a54")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
